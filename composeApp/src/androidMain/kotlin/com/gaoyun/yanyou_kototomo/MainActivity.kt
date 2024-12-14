a092517f5f9080d2f2bc15e8b064142220f7382c
artem.zasypalov <gaoyundexinmen@gmail.com>
2024-12-14 21:15:04 +0100

    Config changes

--- composeApp/src/androidMain/kotlin/com/gaoyun/yanyou_kototomo/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/gaoyun/yanyou_kototomo/MainActivity.kt
@@ -5,13 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import moe.tlaster.precompose.PreComposeApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            YanYouKotoTomoApp()
+            PreComposeApp {
+                YanYouKotoTomoApp()
+            }
         }
     }
 }
