021bfcc441922c885fcbd653dc8924decc1afca7
artem.zasypalov <gaoyundexinmen@gmail.com>
2024-12-26 17:30:14 +0100

    Split screen to components

--- composeApp/src/commonMain/kotlin/com/gaoyun/yanyou_kototomo/ui/quiz_session_summary/QuizSessionSummaryScreen.kt
+++ composeApp/src/commonMain/kotlin/com/gaoyun/yanyou_kototomo/ui/quiz_session_summary/QuizSessionSummaryScreen.kt
@@ -1,24 +1,16 @@
 package com.gaoyun.yanyou_kototomo.ui.quiz_session_summary
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DataUsage
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -27,10 +19,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.gaoyun.yanyou_kototomo.ui.base.composables.AutoResizeText
@@ -41,12 +30,9 @@ import com.gaoyun.yanyou_kototomo.ui.base.composables.SurfaceScaffold
 import com.gaoyun.yanyou_kototomo.ui.base.navigation.BackNavigationEffect
 import com.gaoyun.yanyou_kototomo.ui.base.navigation.NavigationSideEffect
 import com.gaoyun.yanyou_kototomo.ui.base.navigation.QuizSessionSummaryArgs
-import com.gaoyun.yanyou_kototomo.ui.base.theme.YanYouColors
+import com.gaoyun.yanyou_kototomo.ui.quiz_session_summary.components.QuizSessionSummaryQuestionResult
+import com.gaoyun.yanyou_kototomo.ui.quiz_session_summary.components.QuizSessionSummaryStatistics
 import moe.tlaster.precompose.koin.koinViewModel
-import org.jetbrains.compose.resources.painterResource
-import yanyou_kototomo.composeapp.generated.resources.Res
-import yanyou_kototomo.composeapp.generated.resources.correct
-import yanyou_kototomo.composeapp.generated.resources.wrong
 
 @Composable
 fun QuizSessionSummaryScreen(
@@ -73,9 +59,6 @@ fun QuizSessionSummaryScreenContent(
 ) {
     viewState?.let {
         val session = it.session ?: return@let
-        val correctAnswers = session.results.count { it.isCorrect }
-        val wrongAnswers = session.results.size - correctAnswers
-        val correctAnswersPercentage = (correctAnswers.toDouble() / session.results.size.toDouble() * 100).toInt()
 
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
             Column {
@@ -102,45 +85,7 @@ fun QuizSessionSummaryScreenContent(
                                 modifier = Modifier.padding(top = 24.dp)
                             )
                         }
-                        item {
-                            Row(
-                                horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.CenterHorizontally),
-                                verticalAlignment = Alignment.CenterVertically,
-                                modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)
-                            ) {
-                                Icon(Icons.Default.DataUsage, "", Modifier.size(40.dp).padding(top = 3.dp))
-
-                                Text(
-                                    text = "${correctAnswersPercentage}%",
-                                    style = MaterialTheme.typography.displayMedium.copy(fontWeight = FontWeight.SemiBold)
-                                )
-
-                                Spacer(Modifier.width(12.dp))
-
-                                Column(modifier = Modifier.padding(top = 3.dp)) {
-                                    Text(
-                                        text = "$correctAnswers correct",
-                                        style = MaterialTheme.typography.titleMedium.copy(
-                                            color = YanYouColors.current.greenCorrect,
-                                            fontWeight = FontWeight.Medium
-                                        )
-                                    )
-
-                                    Divider(
-                                        height = 1.dp,
-                                        modifier = Modifier.width(56.dp).alpha(0.5f).padding(top = 2.dp).align(Alignment.CenterHorizontally)
-                                    )
-
-                                    Text(
-                                        text = "$wrongAnswers wrong",
-                                        style = MaterialTheme.typography.titleMedium.copy(
-                                            color = YanYouColors.current.redWrong,
-                                            fontWeight = FontWeight.Medium
-                                        )
-                                    )
-                                }
-                            }
-                        }
+                        item { QuizSessionSummaryStatistics(session) }
                         item {
                             Box(modifier = Modifier.fillMaxWidth().padding(top = 8.dp), contentAlignment = Alignment.Center) {
                                 Divider(height = 2.dp, modifier = Modifier.fillMaxWidth(0.8f))
@@ -154,51 +99,7 @@ fun QuizSessionSummaryScreenContent(
                             )
                         }
                         itemsIndexed(session.results) { index, result ->
-                            Column(
-                                modifier = Modifier.fillMaxWidth().padding(horizontal = 4.dp),
-                                verticalArrangement = Arrangement.spacedBy(8.dp)
-                            ) {
-                                Box(contentAlignment = Alignment.CenterStart, modifier = Modifier.fillMaxWidth()) {
-                                    Row(
-                                        horizontalArrangement = Arrangement.spacedBy(4.dp),
-                                        verticalAlignment = Alignment.CenterVertically,
-                                    ) {
-                                        Text(
-                                            text = result.card.front,
-                                            style = MaterialTheme.typography.headlineSmall.copy(fontWeight = FontWeight.Medium),
-                                            maxLines = 1,
-                                        )
-                                        Text(
-                                            text = result.card.translationOrEmpty("– "),
-                                            style = MaterialTheme.typography.titleSmall,
-                                            overflow = TextOverflow.Ellipsis,
-                                            maxLines = 1,
-                                            modifier = Modifier.weight(1f).padding(end = 36.dp)
-                                        )
-                                    }
-
-                                    if (result.isCorrect) {
-                                        Image(
-                                            painter = painterResource(Res.drawable.correct),
-                                            contentDescription = "",
-                                            modifier = Modifier.size(32.dp).align(Alignment.CenterEnd)
-                                        )
-                                    } else {
-                                        Image(
-                                            painter = painterResource(Res.drawable.wrong),
-                                            contentDescription = "",
-                                            modifier = Modifier.size(32.dp).padding(4.dp).align(Alignment.CenterEnd)
-                                        )
-                                    }
-                                }
-
-                                if (index != session.results.lastIndex) {
-                                    Divider(height = 1.dp)
-                                } else {
-                                    Spacer(Modifier.size(32.dp))
-                                }
-
-                            }
+                            QuizSessionSummaryQuestionResult(index, session.results.lastIndex, result)
                         }
                     }
                 }
