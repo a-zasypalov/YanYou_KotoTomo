dc566eaaae62875da8c4a1e05faaf4e23ad46f7b
artem.zasypalov <artem.zasypalov@yazio.com>
2024-12-26 17:30:14 +0100

    Split screen to components

---
+++ composeApp/src/commonMain/kotlin/com/gaoyun/yanyou_kototomo/ui/quiz_session_summary/components/QuizSessionSummaryQuestionResult.kt
@@ -0,0 +1,74 @@
+package com.gaoyun.yanyou_kototomo.ui.quiz_session_summary.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.gaoyun.yanyou_kototomo.data.local.QuizCardResult
+import com.gaoyun.yanyou_kototomo.ui.base.composables.Divider
+import org.jetbrains.compose.resources.painterResource
+import yanyou_kototomo.composeapp.generated.resources.Res
+import yanyou_kototomo.composeapp.generated.resources.correct
+import yanyou_kototomo.composeapp.generated.resources.wrong
+
+@Composable
+fun QuizSessionSummaryQuestionResult(index: Int, lastIndex: Int, result: QuizCardResult) {
+    Column(
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 4.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Box(contentAlignment = Alignment.CenterStart, modifier = Modifier.fillMaxWidth()) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = result.card.front,
+                    style = MaterialTheme.typography.headlineSmall.copy(fontWeight = FontWeight.Medium),
+                    maxLines = 1,
+                )
+                Text(
+                    text = result.card.translationOrEmpty("– "),
+                    style = MaterialTheme.typography.titleSmall,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    modifier = Modifier.weight(1f).padding(end = 36.dp)
+                )
+            }
+
+            if (result.isCorrect) {
+                Image(
+                    painter = painterResource(Res.drawable.correct),
+                    contentDescription = "",
+                    modifier = Modifier.size(32.dp).align(Alignment.CenterEnd)
+                )
+            } else {
+                Image(
+                    painter = painterResource(Res.drawable.wrong),
+                    contentDescription = "",
+                    modifier = Modifier.size(32.dp).padding(4.dp).align(Alignment.CenterEnd)
+                )
+            }
+        }
+
+        if (index != lastIndex) {
+            Divider(height = 1.dp)
+        } else {
+            Spacer(Modifier.size(32.dp))
+        }
+
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/gaoyun/yanyou_kototomo/ui/quiz_session_summary/components/QuizSessionSummaryStatistics.kt
@@ -0,0 +1,69 @@
+package com.gaoyun.yanyou_kototomo.ui.quiz_session_summary.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DataUsage
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.gaoyun.yanyou_kototomo.data.local.QuizSessionWithCards
+import com.gaoyun.yanyou_kototomo.ui.base.composables.Divider
+import com.gaoyun.yanyou_kototomo.ui.base.theme.YanYouColors
+
+@Composable
+fun QuizSessionSummaryStatistics(session: QuizSessionWithCards) {
+    val correctAnswers = session.results.count { it.isCorrect }
+    val wrongAnswers = session.results.size - correctAnswers
+    val correctAnswersPercentage = (correctAnswers.toDouble() / session.results.size.toDouble() * 100).toInt()
+
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.CenterHorizontally),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)
+    ) {
+        Icon(Icons.Default.DataUsage, "", Modifier.size(40.dp).padding(top = 3.dp))
+
+        Text(
+            text = "${correctAnswersPercentage}%",
+            style = MaterialTheme.typography.displayMedium.copy(fontWeight = FontWeight.SemiBold)
+        )
+
+        Spacer(Modifier.width(12.dp))
+
+        Column(modifier = Modifier.padding(top = 3.dp)) {
+            Text(
+                text = "$correctAnswers correct",
+                style = MaterialTheme.typography.titleMedium.copy(
+                    color = YanYouColors.current.greenCorrect,
+                    fontWeight = FontWeight.Medium
+                )
+            )
+
+            Divider(
+                height = 1.dp,
+                modifier = Modifier.width(56.dp).alpha(0.5f).padding(top = 2.dp).align(Alignment.CenterHorizontally)
+            )
+
+            Text(
+                text = "$wrongAnswers wrong",
+                style = MaterialTheme.typography.titleMedium.copy(
+                    color = YanYouColors.current.redWrong,
+                    fontWeight = FontWeight.Medium
+                )
+            )
+        }
+    }
+}
