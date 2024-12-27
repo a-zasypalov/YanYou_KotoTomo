f04727ecb0d3d6aac966c5e45ae9d77ded5fd217
artem.zasypalov <gaoyundexinmen@gmail.com>
2024-12-27 17:07:16 +0100

    Add statistics overview screen

---
+++ composeApp/src/commonMain/kotlin/com/gaoyun/yanyou_kototomo/ui/statistics/components/CardProgressStatisticsItem.kt
@@ -0,0 +1,70 @@
+package com.gaoyun.yanyou_kototomo.ui.statistics.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.EventRepeat
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.gaoyun.yanyou_kototomo.data.local.CardSimpleDataEntryWithProgress
+import com.gaoyun.yanyou_kototomo.util.toRelativeFormat
+
+@Composable
+fun CardProgressStatisticsItem(card: CardSimpleDataEntryWithProgress) {
+    Surface(
+        modifier = Modifier.fillMaxWidth(),
+        shape = MaterialTheme.shapes.medium,
+        tonalElevation = 8.dp,
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                Text(
+                    text = card.character,
+                    style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Medium),
+                )
+                Text(
+                    text = "(${card.answer})(${card.answer})(${card.answer})(${card.answer})(${card.answer})(${card.answer})(${card.answer})(${card.answer})(${card.answer})",
+                    style = MaterialTheme.typography.titleSmall,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                )
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.EventRepeat,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f),
+                    modifier = Modifier.size(16.dp)
+                )
+                Text(
+                    text = card.progress.nextReview.toRelativeFormat(),
+                    style = MaterialTheme.typography.bodySmall,
+                )
+            }
+        }
+    }
+}
