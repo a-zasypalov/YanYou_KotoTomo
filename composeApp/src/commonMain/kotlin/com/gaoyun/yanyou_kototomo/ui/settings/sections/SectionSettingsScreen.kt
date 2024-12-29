88b7df56493dfad9bd4263b68c28665ab1ed9723
artem.zasypalov <artem.zasypalov@yazio.com>
2024-12-29 23:27:08 +0100

    Add settings sections screen template

--- composeApp/src/commonMain/kotlin/com/gaoyun/yanyou_kototomo/ui/settings/sections/SectionSettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/gaoyun/yanyou_kototomo/ui/settings/sections/SectionSettingsScreen.kt
@@ -1,7 +1,19 @@
 package com.gaoyun.yanyou_kototomo.ui.settings.sections
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.gaoyun.yanyou_kototomo.ui.base.composables.AutoResizeText
+import com.gaoyun.yanyou_kototomo.ui.base.composables.FontSizeRange
 import com.gaoyun.yanyou_kototomo.ui.base.composables.SurfaceScaffold
 import com.gaoyun.yanyou_kototomo.ui.base.navigation.BackNavigationEffect
 import com.gaoyun.yanyou_kototomo.ui.base.navigation.NavigationSideEffect
@@ -22,10 +34,29 @@ fun SectionSettingsScreen(
     SurfaceScaffold(
         backHandler = { navigate(BackNavigationEffect) },
     ) {
-        when (section) {
-            SettingsSections.AppIcon -> {}
-            SettingsSections.ColorTheme -> {}
-            SettingsSections.AboutApp -> {}
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            AutoResizeText(
+                text = section.title(),
+                fontSizeRange = FontSizeRange(min = 16.sp, max = MaterialTheme.typography.displayLarge.fontSize),
+                style = MaterialTheme.typography.displayLarge,
+                maxLines = 1,
+                modifier = Modifier.fillMaxWidth().wrapContentHeight(align = Alignment.CenterVertically)
+            )
+
+            when (section) {
+                SettingsSections.AppIcon -> {}
+                SettingsSections.ColorTheme -> {}
+                SettingsSections.AboutApp -> {}
+            }
         }
     }
+}
+
+private fun SettingsSections.title() = when (this) {
+    SettingsSections.AppIcon -> "App icon"
+    SettingsSections.ColorTheme -> "Color theme"
+    SettingsSections.AboutApp -> "About app"
 }
