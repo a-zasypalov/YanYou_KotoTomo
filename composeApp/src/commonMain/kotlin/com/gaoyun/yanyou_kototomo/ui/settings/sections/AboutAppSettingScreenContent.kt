9ac2d05c1f9d4b8990c9c24a1c7296ba44e423ee
artem.zasypalov <gaoyundexinmen@gmail.com>
2024-12-30 21:24:34 +0100

    Fix about screen

--- composeApp/src/commonMain/kotlin/com/gaoyun/yanyou_kototomo/ui/settings/sections/AboutAppSettingScreenContent.kt
+++ composeApp/src/commonMain/kotlin/com/gaoyun/yanyou_kototomo/ui/settings/sections/AboutAppSettingScreenContent.kt
@@ -1,11 +1,14 @@
 package com.gaoyun.yanyou_kototomo.ui.settings.sections
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -23,12 +26,13 @@ import yanyou_kototomo.composeapp.generated.resources.wave
 
 @Composable
 fun AboutAppSettingScreenContent() {
-    LazyColumn(modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-        item {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp).navigationBarsPadding().padding(bottom = 64.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Spacer(modifier = Modifier.size(32.dp))
-        }
 
-        item {
             Surface(shadowElevation = 16.dp, shape = MaterialTheme.shapes.medium) {
                 Image(
                     painter = painterResource(Res.drawable.app_icon_variant_1),
@@ -36,40 +40,29 @@ fun AboutAppSettingScreenContent() {
                     modifier = Modifier.size(96.dp)
                 )
             }
-        }
 
-        item {
             Spacer(modifier = Modifier.size(32.dp))
-        }
 
-        item {
             Text(
                 text = "言友",
                 style = MaterialTheme.typography.displayMedium.copy(fontWeight = FontWeight.SemiBold),
             )
-        }
 
-        item {
             Spacer(modifier = Modifier.size(4.dp))
-        }
 
-        item {
             Text(
                 text = "HieroFlash",
                 style = MaterialTheme.typography.titleLarge,
                 color = MaterialTheme.colorScheme.onSurface
             )
-        }
 
-        item {
             Spacer(modifier = Modifier.size(32.dp))
-        }
 
-        item {
             Surface(
                 shadowElevation = 4.dp,
                 tonalElevation = 8.dp,
                 shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.weight(1f)
             ) {
                 Text(
                     text = "The original idea for the name of this app was YangYou or KotoTomo. It combines two hieroglyphs and in both Chinese and Japanese they have the same meaning.\n\n言 means \"words\" or \"language\".\n\n友 means \"friend\".\n\nTogether, they represent the idea of a trusted companion to help you master words and characters in Chinese and Japanese alongside with official HSK and JLPT books.",
@@ -80,21 +73,11 @@ fun AboutAppSettingScreenContent() {
             }
         }
 
-        item {
-            Spacer(modifier = Modifier.size(32.dp))
-        }
-
-        item {
-            Image(
-                painter = painterResource(Res.drawable.wave),
-                contentDescription = null,
-                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
-                modifier = Modifier.size(48.dp).alpha(0.4f)
-            )
-        }
-
-        item {
-            Spacer(modifier = Modifier.size(32.dp))
-        }
+        Image(
+            painter = painterResource(Res.drawable.wave),
+            contentDescription = null,
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
+            modifier = Modifier.align(Alignment.BottomCenter).navigationBarsPadding().size(48.dp).alpha(0.4f)
+        )
     }
 }
