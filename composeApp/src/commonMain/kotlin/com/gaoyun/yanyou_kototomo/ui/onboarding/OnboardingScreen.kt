4fd4cc7fa339a0476fcb3e06141767625d847b36
artem.zasypalov <artem.zasypalov@yazio.com>
2025-01-01 21:27:55 +0100

    Add onboarding ui

--- composeApp/src/commonMain/kotlin/com/gaoyun/yanyou_kototomo/ui/onboarding/OnboardingScreen.kt
+++ composeApp/src/commonMain/kotlin/com/gaoyun/yanyou_kototomo/ui/onboarding/OnboardingScreen.kt
@@ -1,31 +1,24 @@
 package com.gaoyun.yanyou_kototomo.ui.onboarding
 
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.gaoyun.yanyou_kototomo.data.local.LanguageId
-import com.gaoyun.yanyou_kototomo.ui.base.composables.PrimaryElevatedButton
 import com.gaoyun.yanyou_kototomo.ui.base.composables.SurfaceScaffold
 import com.gaoyun.yanyou_kototomo.ui.base.navigation.NavigationSideEffect
 import com.gaoyun.yanyou_kototomo.ui.base.navigation.ToHomeScreen
+import com.gaoyun.yanyou_kototomo.ui.onboarding.pages.OnboardingCardShowcasePage
 import com.gaoyun.yanyou_kototomo.ui.onboarding.pages.OnboardingLanguageChooserPage
+import com.gaoyun.yanyou_kototomo.ui.onboarding.pages.OnboardingLearningBookmarksPage
+import com.gaoyun.yanyou_kototomo.ui.onboarding.pages.OnboardingReviewQuizPage
 import com.gaoyun.yanyou_kototomo.ui.onboarding.pages.OnboardingWelcomePage
 import kotlinx.coroutines.launch
 import moe.tlaster.precompose.koin.koinViewModel
@@ -51,52 +44,33 @@ private fun OnboardingScreenContent(
     onPrimaryLanguageChosen: (LanguageId) -> Unit,
     onFinish: () -> Unit,
 ) {
-    val state = rememberPagerState(pageCount = { return@rememberPagerState 3 })
+    val state = rememberPagerState(pageCount = { return@rememberPagerState 5 })
     val scope = rememberCoroutineScope()
 
-    Box {
-        HorizontalPager(
-            state = state,
-            modifier = Modifier
-                .padding(WindowInsets.systemBars.asPaddingValues())
-                .padding(bottom = 72.dp)
-        ) { page ->
-            when (page) {
-                0 -> OnboardingWelcomePage()
-                1 -> OnboardingLanguageChooserPage(onPrimaryLanguageChosen)
-                2 -> Box(modifier = Modifier.fillMaxSize())
-            }
+    fun nextPage() {
+        scope.launch {
+            state.animateScrollToPage(
+                page = state.currentPage + 1,
+                animationSpec = tween(durationMillis = 500)
+            )
         }
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.BottomCenter)
-                .padding(bottom = 16.dp)
+    }
+
+    HorizontalPager(
+        state = state,
+        modifier = Modifier.navigationBarsPadding().statusBarsPadding(),
+        userScrollEnabled = false
+    ) { page ->
+        Box(
+            modifier = Modifier.fillMaxSize()
         ) {
-            DotsIndicator(
-                totalDots = state.pageCount,
-                selectedIndex = state.currentPage,
-                selectedColor = MaterialTheme.colorScheme.inverseSurface,
-                unSelectedColor = MaterialTheme.colorScheme.outline,
-            )
-            Spacer(modifier = Modifier.size(16.dp))
-            if (state.currentPage < state.pageCount - 1) {
-                PrimaryElevatedButton(
-                    text = "Next",
-                    onClick = { scope.launch { state.animateScrollToPage(state.currentPage + 1) } },
-                    modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
-                )
-            } else {
-                PrimaryElevatedButton(
-                    text = "Start",
-                    onClick = onFinish,
-                    modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
-                )
+            when (page) {
+                0 -> OnboardingWelcomePage { nextPage() }
+                1 -> OnboardingLanguageChooserPage { onPrimaryLanguageChosen(it); nextPage() }
+                2 -> OnboardingLearningBookmarksPage { nextPage() }
+                3 -> OnboardingReviewQuizPage { nextPage() }
+                4 -> OnboardingCardShowcasePage(onFinish)
             }
-            Spacer(
-                modifier = Modifier.navigationBarsPadding().size(16.dp)
-            )
         }
     }
 }
--- composeApp/src/commonMain/kotlin/com/gaoyun/yanyou_kototomo/ui/onboarding/pages/OnboardingWelcomePage.kt
+++ composeApp/src/commonMain/kotlin/com/gaoyun/yanyou_kototomo/ui/onboarding/pages/OnboardingWelcomePage.kt
@@ -1,6 +1,7 @@
 package com.gaoyun.yanyou_kototomo.ui.onboarding.pages
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,66 +19,86 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.gaoyun.yanyou_kototomo.ui.base.composables.PrimaryElevatedButton
 import org.jetbrains.compose.resources.painterResource
 import yanyou_kototomo.composeapp.generated.resources.Res
 import yanyou_kototomo.composeapp.generated.resources.app_icon_variant_1
 
 @Composable
-fun OnboardingWelcomePage() {
-    Column(
-        modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp).navigationBarsPadding().padding(bottom = 64.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Spacer(modifier = Modifier.size(32.dp))
-
-        Surface(shadowElevation = 16.dp, shape = MaterialTheme.shapes.large) {
-            Image(
-                painter = painterResource(Res.drawable.app_icon_variant_1),
-                contentDescription = null,
-                modifier = Modifier.size(96.dp)
-            )
-        }
-
-        Spacer(modifier = Modifier.size(32.dp))
-
-        Text(
-            text = "言友",
-            style = MaterialTheme.typography.displayMedium.copy(fontWeight = FontWeight.SemiBold),
-        )
-
-        Spacer(modifier = Modifier.size(4.dp))
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        Text(
-            text = "Welcome to HieroFlash",
-            style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Medium),
-            color = MaterialTheme.colorScheme.onSurface
-        )
-
-        Text(
-            text = "Your trusted companion to master words and characters from HSK and JLPT books.",
-            style = MaterialTheme.typography.bodyLarge,
-            color = MaterialTheme.colorScheme.onSurface,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth().padding(16.dp),
-        )
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        Surface(
-            shadowElevation = 4.dp,
-            tonalElevation = 8.dp,
-            shape = MaterialTheme.shapes.medium,
+fun OnboardingWelcomePage(onNextPage: () -> Unit) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp).navigationBarsPadding().padding(bottom = 64.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Column(modifier = Modifier.padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(
-                    text = "言 - words, language.\n友 - friend.",
-                    style = MaterialTheme.typography.titleSmall,
-                    fontStyle = FontStyle.Italic,
-                    color = MaterialTheme.colorScheme.onSurface,
+            Spacer(modifier = Modifier.size(32.dp))
+
+            Surface(shadowElevation = 16.dp, shape = MaterialTheme.shapes.large) {
+                Image(
+                    painter = painterResource(Res.drawable.app_icon_variant_1),
+                    contentDescription = null,
+                    modifier = Modifier.size(96.dp)
                 )
             }
+
+            Spacer(modifier = Modifier.size(32.dp))
+
+            Text(
+                text = "言友",
+                style = MaterialTheme.typography.displayMedium.copy(fontWeight = FontWeight.SemiBold),
+            )
+
+            Spacer(modifier = Modifier.size(4.dp))
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Text(
+                text = "Welcome to HieroFlash",
+                style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Medium),
+                color = MaterialTheme.colorScheme.onSurface
+            )
+
+            Text(
+                text = "Your trusted companion to master words and characters from HSK and JLPT books.",
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onSurface,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth().padding(16.dp),
+            )
+
+            Spacer(modifier = Modifier.weight(1.75f))
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 16.dp)
+        ) {
+            Surface(
+                shadowElevation = 4.dp,
+                tonalElevation = 8.dp,
+                shape = MaterialTheme.shapes.medium,
+            ) {
+                Column(modifier = Modifier.padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(
+                        text = "言 - words, language.\n友 - friend.",
+                        style = MaterialTheme.typography.titleSmall,
+                        fontStyle = FontStyle.Italic,
+                        color = MaterialTheme.colorScheme.onSurface,
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.size(16.dp))
+
+            PrimaryElevatedButton(
+                text = "Next",
+                onClick = onNextPage,
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+            )
+
+            Spacer(modifier = Modifier.navigationBarsPadding().size(16.dp))
         }
     }
 }
