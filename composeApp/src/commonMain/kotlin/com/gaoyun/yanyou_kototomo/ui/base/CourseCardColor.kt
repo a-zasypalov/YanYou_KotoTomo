464e6eceed90a65739882094017e7dd5f8179e5e
artem.zasypalov <gaoyundexinmen@gmail.com>
2025-01-04 00:30:59 +0100

    Add genki v1 kana decks

--- composeApp/src/commonMain/kotlin/com/gaoyun/yanyou_kototomo/ui/base/CourseCardColor.kt
+++ composeApp/src/commonMain/kotlin/com/gaoyun/yanyou_kototomo/ui/base/CourseCardColor.kt
@@ -4,7 +4,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import com.gaoyun.yanyou_kototomo.data.local.CourseId
-import com.gaoyun.yanyou_kototomo.data.local.course.Course
 
 @Composable
 fun CourseId.courseCardColor(): Color = when (identifier) {
@@ -14,6 +13,7 @@ fun CourseId.courseCardColor(): Color = when (identifier) {
     "hsk4_en" -> Color(0xFFB61F26)
     "hsk5_en" -> Color(0xFF193F76)
     "hsk6_en" -> Color(0xFF641F62)
+    "genki1_en" -> Color(0xFF0331B3)
     "jlpt5_en" -> Color(0xFF0331B3)
     "jlpt4_en" -> Color(0xFF01A79C)
     "jlpt3_en" -> Color(0xFF90C122)
