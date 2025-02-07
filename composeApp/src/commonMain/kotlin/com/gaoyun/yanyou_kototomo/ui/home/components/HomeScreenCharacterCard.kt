9109abb3082d376c3bde490fe281c566fa2ade91
artem.zasypalov <artem.zasypalov@yazio.com>
2025-02-07 23:59:11 +0100

    Add dots to home screen

--- composeApp/src/commonMain/kotlin/com/gaoyun/yanyou_kototomo/ui/home/components/HomeScreenCharacterCard.kt
+++ composeApp/src/commonMain/kotlin/com/gaoyun/yanyou_kototomo/ui/home/components/HomeScreenCharacterCard.kt
@@ -1,10 +1,14 @@
 package com.gaoyun.yanyou_kototomo.ui.home.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
@@ -16,6 +20,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.gaoyun.yanyou_kototomo.data.local.LanguageId
 import com.gaoyun.yanyou_kototomo.data.local.card.CardWithProgress
+import com.gaoyun.yanyou_kototomo.domain.mapIntervalToColor
 import com.gaoyun.yanyou_kototomo.ui.base.composables.platformStyleClickable
 
 @Composable
@@ -24,16 +29,21 @@ fun HomeScreenCharacterCard(card: CardWithProgress<*>, languageId: LanguageId, o
         modifier = Modifier.fillMaxWidth().platformStyleClickable(onClick = { onClick(card, languageId) }),
         elevation = CardDefaults.elevatedCardElevation(defaultElevation = 2.dp)
     ) {
-        Column(
-            modifier = Modifier.fillMaxSize().padding(8.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = card.card.front,
-                style = MaterialTheme.typography.displayMedium,
-                textAlign = TextAlign.Center,
-            )
+        Box(modifier = Modifier.fillMaxSize().padding(6.dp), contentAlignment = Alignment.TopEnd) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(
+                    text = card.card.front,
+                    style = MaterialTheme.typography.displayMedium,
+                    textAlign = TextAlign.Center,
+                )
+            }
+            card.progress?.interval?.let {
+                Box(modifier = Modifier.size(6.dp).background(color = mapIntervalToColor(it), shape = CircleShape))
+            } ?: Box(modifier = Modifier.size(6.dp))
         }
     }
 }
